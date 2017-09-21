813e6f8e8eb8e030e9f892c3ecf72703427f989c
andrey demidov <demas@fastmail.com>
2017-09-21 17:01:58 +0300

    Split FeedsComponent to separate files

--- src/app/settings.feed/feeds.component.ts
+++ src/app/settings.feed/feeds.component.ts
@@ -9,46 +9,8 @@ import {ActivatedRoute, Router} from '@angular/router';
 @Component({
   moduleId: module.id,
   selector: 'feeds',
-  template:
-    `
-    <div class="feed-section container-fluid">
-            
-        <div class="row col-md-12 feed-panel">          
-          <button type="button" class="btn btn-default btn-xs" (click)="subscribe()">
-            Subscribe            
-          </button>
-          <input #filterbox type="text" id="feed-search" (keyup)="onFilter(filterbox.value)">
-        </div>            
-            
-        <div *ngFor="let feedType of feedTypes" class="row col-md-12">
-
-          <div class="row col-md-12">{{feedType}}</div>                    
-                    
-          <div *ngFor="let feed of feedsByType(feedType)" class="row col-md-12">
-            <a [routerLink]="['/feeds', feed.Id]" [ngClass]="{'broken': feed.Broken == 1}">{{feed.Title}}</a> ({{feed.LastSyncTime | stringdate | cleandate }})
-          </div>
-        </div>                       
-    </div>
-  `,
-  styles: [`              
-                   
-    a { font-size: 13px; }
-                       
-    .feed-section {    
-       margin-top: 4px;
-       margin-left: 30px;
-       margin-right: 30px;
-       font-size: 13px;
-    }
-    
-    .feed-panel {
-      margin-top: 6px;
-      margin-bottom: 6px;
-    }
-    
-    a.broken { color: red; }
-         
-`],
+  templateUrl: 'feeds.component.html',
+  styleUrls: ['feeds.component.css'],
   providers: [RssService]
 })
 
